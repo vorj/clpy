76a7c193df7df788dad2f61c295a27e19d0cec41
Tomoharu Kitawaki <tomoharu.kitawaki@fixstars.com>
2018-07-17 15:15:11 +0900

    reformat code for flake8

--- tests/clpy_tests/opencl_tests/test_linalg.py
+++ tests/clpy_tests/opencl_tests/test_linalg.py
@@ -34,10 +34,12 @@ class TestProduct(unittest.TestCase):
     def test_tensordot(self):
         npA = numpy.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
                            [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
-                           [[19, 20, 21], [22, 23, 24], [25, 26, 27]]], dtype='float32')
+                           [[19, 20, 21], [22, 23, 24], [25, 26, 27]]],
+                          dtype='float32')
         npB = numpy.array([[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
                            [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
-                           [[19, 20, 21], [22, 23, 24], [25, 26, 27]]], dtype='float32')
+                           [[19, 20, 21], [22, 23, 24], [25, 26, 27]]],
+                          dtype='float32')
         expectedC = numpy.tensordot(npA, npB)
 
         clpA = clpy.array(npA)
