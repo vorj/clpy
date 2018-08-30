0a56f299b8c130257455bcf111aa3c5646e50feb
Naoya Sakabe <40021205+nsakabe-fixstars@users.noreply.github.com>
2018-08-30 15:35:26 +0900

    Device index can be specified

--- clpy/backend/opencl/verassert/verassert.cpp
+++ clpy/backend/opencl/verassert/verassert.cpp
@@ -35,14 +35,26 @@ version_t parse_version_str(std::string const& str){
 
 
 int main(int argc, char const** argv){
-  if (argc != 2) {
-    std::cerr << "Usage: " << argv[0] << " <OpenCL Version>\nExample: " << argv[0] << " 1.2" << std::endl;
+  int specified_device_index = -1;
+
+  if (argc < 2 || 3 < argc) {
+    std::cerr << "Usage: \n  " <<
+      argv[0] << " <Version>\n  " <<
+      argv[0] << " <Version> <Device Index>\n" <<
+      "Example: " << argv[0] << "  1.2       Checks Platform#0 >= 1.2\n" <<
+      "         " << argv[0] << "  1.2  2    Checks Platform#0 and Device#2 >= 1.2" <<
+      std::endl;
+
+    std::cerr << "\nReturns 0 if the conditions are satisfied, else returns 1." << std::endl;
     exit (1);
   }
   auto const capable_version_str = std::string("OpenCL ") + argv[1];
   version_t const capable_version = parse_version_str(capable_version_str);
+  if (argc == 3){
+    specified_device_index = std::atoi(argv[2]);
+  }
 
-  // OpenCL C Version
+  // OpenCL C Version Check
 #ifndef CL_VERSION_1_2
 #define CL_VERSION_1_2 0
 #endif
@@ -50,12 +62,12 @@ int main(int argc, char const** argv){
   if (CL_VERSION_1_2 == 1){
     std::cout << " OK" << std::endl;
   }else{
-    std::cout << " must be >= " << argv[1] << std::endl;
+    std::cout << " must be >= 1.2" << std::endl;
     return 1;
   }
 
 
-  // Platform Version
+  // Platform Version Check
   cl_uint num_platforms;
   cl_int returncode = clGetPlatformIDs(0, NULL, &num_platforms);
   check_success(returncode);
@@ -82,29 +94,36 @@ int main(int argc, char const** argv){
   }
 
 
-  // Device Version
-  cl_uint num_devices;
-  returncode = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 0, NULL, &num_devices);
-  check_success(returncode);
+  if (specified_device_index >= 0){
+    // Device Version Check
+    cl_uint num_devices;
+    returncode = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, 0, NULL, &num_devices);
+    check_success(returncode);
 
-  auto devices = std::unique_ptr<cl_device_id[]>(new cl_device_id[num_devices]);
-  returncode = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, num_devices, devices.get(), &num_devices);
-  check_success(returncode);
+    if(specified_device_index >= static_cast<int>(num_devices)){
+      std::cerr << "Device #" << specified_device_index << " does not exist." << std::endl;
+      return 1;
+    }
 
-  returncode = clGetDeviceInfo(devices[0], CL_DEVICE_VERSION, 0, NULL, &param_value_size);
-  check_success(returncode);
+    auto devices = std::unique_ptr<cl_device_id[]>(new cl_device_id[num_devices]);
+    returncode = clGetDeviceIDs(platform, CL_DEVICE_TYPE_DEFAULT, num_devices, devices.get(), &num_devices);
+    check_success(returncode);
 
-  auto device_version_str = std::unique_ptr<char[]>(new char[param_value_size]);
-  returncode = clGetDeviceInfo(devices[0], CL_DEVICE_VERSION, param_value_size, device_version_str.get(), &param_value_size);
-  check_success(returncode);
+    returncode = clGetDeviceInfo(devices[specified_device_index], CL_DEVICE_VERSION, 0, NULL, &param_value_size);
+    check_success(returncode);
 
-  auto const device_version = parse_version_str(std::string(device_version_str.get()));
-  std::cout << "Device Version: " << device_version.first << "." << device_version.second;
-  if (device_version >= capable_version){
-    std::cout << " OK" << std::endl;
-  }else{
-    std::cout << " must be >= " << argv[1] << std::endl;
-    return 1;
+    auto device_version_str = std::unique_ptr<char[]>(new char[param_value_size]);
+    returncode = clGetDeviceInfo(devices[specified_device_index], CL_DEVICE_VERSION, param_value_size, device_version_str.get(), &param_value_size);
+    check_success(returncode);
+
+    auto const device_version = parse_version_str(std::string(device_version_str.get()));
+    std::cout << "Device #" << specified_device_index << " Version: " << device_version.first << "." << device_version.second;
+    if (device_version >= capable_version){
+      std::cout << " OK" << std::endl;
+    }else{
+      std::cout << " must be >= " << argv[1] << std::endl;
+      return 1;
+    }
   }
 
 
