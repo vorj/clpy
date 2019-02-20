1d8afce3a83df1c38d2d91311ab95cab19a1f440
Naoya Sakabe <40021205+nsakabe-fixstars@users.noreply.github.com>
2019-02-20 16:51:05 +0900

    enable _scatter_update_kernel

--- clpy/core/core.pyx
+++ clpy/core/core.pyx
@@ -2770,17 +2770,17 @@ cdef _choose_clip_kernel = ElementwiseKernel(
     'clpy_choose_clip')
 
 
-# cdef _scatter_update_kernel = ElementwiseKernel(
-#     'T v, S indices, int32 cdim, int32 rdim, int32 adim',
-#     'raw T a',
-#     '''
-#       S wrap_indices = indices % adim;
-#       if (wrap_indices < 0) wrap_indices += adim;
-#       ptrdiff_t li = i / (rdim * cdim);
-#       ptrdiff_t ri = i % rdim;
-#       a[(li * adim + wrap_indices) * rdim + ri] = v;
-#     ''',
-#     'clpy_scatter_update')
+cdef _scatter_update_kernel = ElementwiseKernel(
+    'T v, S indices, int32 cdim, int32 rdim, int32 adim',
+    'raw T a',
+    '''
+      S wrap_indices = indices % adim;
+      if (wrap_indices < 0) wrap_indices += adim;
+      ptrdiff_t li = i / (rdim * cdim);
+      ptrdiff_t ri = i % rdim;
+      a[(li * adim + wrap_indices) * rdim + ri] = v;
+    ''',
+    'clpy_scatter_update')
 
 
 cdef _scatter_add_kernel = ElementwiseKernel(
@@ -2975,9 +2975,8 @@ cpdef _scatter_op_single(ndarray a, ndarray indices, v,
     indices = broadcast_to(indices, v_shape)
 
     if op == 'update':
-        # _scatter_update_kernel(
-        #     v, indices, cdim, rdim, adim, a.reduced_view())
-        raise NotImplementedError("clpy does not support this")
+        _scatter_update_kernel(
+            v, indices, cdim, rdim, adim, a.reduced_view())
     elif op == 'add':
         # NOTE(clpy):
         # CuPy had supported
