99709208c267ea2e43037bce04c0f31e171c074b
Naoya Sakabe <40021205+nsakabe-fixstars@users.noreply.github.com>
2019-03-26 18:10:14 +0900

    install_tests passes

--- jenkins/build_and_test.sh
+++ jenkins/build_and_test.sh
@@ -52,6 +52,10 @@ tests/clpy_tests/math_tests/
 tests/clpy_tests/logic_tests/
 "
 
+TEST_DIRS_IN_ROOT_DIR="
+tests/install_tests/
+"
+
 TEST_FILES="
 tests/clpy_tests/core_tests/test_carray.py
 tests/clpy_tests/core_tests/test_core.py
@@ -107,6 +111,14 @@ for d in $TEST_DIRS; do
     ERROR_HAS_OCCURRED=1
   fi
   popd
+done
+
+for d in $TEST_DIRS_IN_ROOT_DIR; do
+  python -m pytest $d  2>&1 | tee temporary_log
+  if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+    cat temporary_log >> $ERRORS_FILENAME
+    ERROR_HAS_OCCURRED=1
+  fi
 done
 
 for f in $TEST_FILES; do
