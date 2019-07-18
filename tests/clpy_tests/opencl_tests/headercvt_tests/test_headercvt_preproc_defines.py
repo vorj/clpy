867e125c2fd920ebfb3e9676daa2ae7ffc876ea6
Naoya Sakabe <40021205+nsakabe-fixstars@users.noreply.github.com>
2019-07-18 18:36:01 +0900

    Separate tests files for headercvt by the unit of class

---
+++ tests/clpy_tests/opencl_tests/headercvt_tests/test_headercvt_preproc_defines.py
@@ -0,0 +1,25 @@
+import unittest
+
+import headercvt_test_utils as util
+
+class TestHeadercvtPreprocDefines(unittest.TestCase):
+    def setUp(self):
+        util.check_existence_of_headercvt()
+
+    @util.with_temp_wd
+    def test_headercvt_preproc_define_accept_case(self, wd):
+        results = util.kick_headercvt_and_get_results(wd, """
+        #define CL_SOME_VALUE 1
+        """)
+        self.assertTrue(util.contains(
+            results["preprocessor_defines"], "CL_SOME_VALUE"))
+        self.assertTrue(util.compile_with(wd, "print(CL_SOME_VALUE)"))
+
+    @util.with_temp_wd
+    def test_headercvt_preproc_define_decline_case(self, wd):
+        results = util.kick_headercvt_and_get_results(wd, """
+        #define SOME_VALUE 1
+        """)
+        self.assertTrue(not util.contains(
+            results["preprocessor_defines"], "SOME_VALUE"))
+
