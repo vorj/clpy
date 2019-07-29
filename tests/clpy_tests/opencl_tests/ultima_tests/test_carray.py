9f4c8f95a5dcdf30773c807e5362d75a3e3db559
vorj <40021161+vorj@users.noreply.github.com>
2019-07-29 17:57:43 +0900

    add test_carray.py

---
+++ tests/clpy_tests/opencl_tests/ultima_tests/test_carray.py
@@ -0,0 +1,91 @@
+# flake8: noqa
+# TODO(vorj): When we will meet flake8 3.7.0+,
+#               we should ignore only W291 for whole file
+#               using --per-file-ignores .
+
+import unittest
+import utility
+
+
+class TestUltimaCArray(unittest.TestCase):
+
+    def test_carray_argument_mutation(self):
+        x = utility.exec_ultima('', _clpy_header='#include <cupy/carray.hpp>') + '''
+void f(__global int* const __restrict__ arr, const CArray_2 arr_info) 
+{
+}
+'''[1:]
+        y = utility.exec_ultima(
+            '''
+            void f(CArray<int, 2> arr){}
+            ''',
+            _clpy_header='#include <cupy/carray.hpp>')
+        self.maxDiff = None
+        self.assertEqual(x, y)
+
+    def test_carray_member_function(self):
+        x = utility.exec_ultima('', _clpy_header='#include <cupy/carray.hpp>') + '''
+void f(__global int* const __restrict__ arr, const CArray_2 arr_info) 
+{
+    ((const size_t)arr_info.size_);
+    ((const size_t*)arr_info.shape_);
+    ((const size_t*)arr_info.strides_);
+}
+'''[1:]
+        y = utility.exec_ultima(
+            '''
+            void f(CArray<int, 2> arr){
+              arr.size();
+              arr.shape();
+              arr.strides();
+            }
+            ''',
+            _clpy_header='#include <cupy/carray.hpp>')
+        self.maxDiff = None
+        self.assertEqual(x, y)
+
+    def test_carray_0_member_function(self):
+        x = utility.exec_ultima('', _clpy_header='#include <cupy/carray.hpp>') + '''
+void f(__global int* const __restrict__ arr, const CArray_0 arr_info) 
+{
+    ((const size_t)arr_info.size_);
+    ((const size_t*)NULL);
+    ((const size_t*)NULL);
+}
+'''[1:]
+        y = utility.exec_ultima(
+            '''
+            void f(CArray<int, 0> arr){
+              arr.size();
+              arr.shape();
+              arr.strides();
+            }
+            ''',
+            _clpy_header='#include <cupy/carray.hpp>')
+        self.maxDiff = None
+        self.assertEqual(x, y)
+
+    def test_carray_1_member_function(self):
+        x = utility.exec_ultima('', _clpy_header='#include <cupy/carray.hpp>') + '''
+void f(__global int* const __restrict__ arr, const CArray_1 arr_info) 
+{
+    ((const size_t)arr_info.size_);
+    ((const size_t*)&arr_info.shape_);
+    ((const size_t*)&arr_info.strides_);
+}
+'''[1:]
+        y = utility.exec_ultima(
+            '''
+            void f(CArray<int, 1> arr){
+              arr.size();
+              arr.shape();
+              arr.strides();
+            }
+            ''',
+            _clpy_header='#include <cupy/carray.hpp>')
+        self.maxDiff = None
+        self.assertEqual(x, y)
+
+
+if __name__ == "__main__":
+    unittest.main()
