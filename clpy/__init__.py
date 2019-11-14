0f6cbb160027d800d77b47d520aeabed79e22170
Naoya Sakabe <40021205+nsakabe-fixstars@users.noreply.github.com>
2019-11-14 18:01:49 +0900

    Place stub clpy.argsort to enable random_tests

--- clpy/__init__.py
+++ clpy/__init__.py
@@ -487,7 +487,7 @@ from clpy.sorting.search import argmax  # NOQA
 from clpy.sorting.search import argmin  # NOQA
 
 # from clpy.sorting.sort import argpartition  # NOQA
-# from clpy.sorting.sort import argsort  # NOQA
+from clpy.sorting.sort import argsort  # NOQA
 # from clpy.sorting.sort import lexsort  # NOQA
 from clpy.sorting.sort import msort  # NOQA
 # from clpy.sorting.sort import partition  # NOQA
